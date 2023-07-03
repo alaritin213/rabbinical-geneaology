9c1507203ee29c409422960f3418235174701755
art <a@a.com>
2023-07-02 22:40:56 -0400

    f

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
         </nav>
 
         <Routes>
-          <Route path="/rabbis-sephardic" element={<XmlDisplay filename="/data/Rabbis-Sephardic.xml" />} />
+          <Route path="/rabbis-sephardic" element={<XmlDisplay filename="/data/Rishonim.xml" />} />
           <Route path="/rishonim" element={<XmlDisplay filename="/data/Rishonim.xml" />} />
         </Routes>
       </div>
